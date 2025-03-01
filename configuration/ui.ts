7d03c1062948d800b697381b7f49674615f7f905
ajayvikram1991 <ajay.vikram1991@gmail.com>
2025-03-01 04:50:50 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,8 +3,8 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `Welcome to Ajay's chatbot. How can I help you today ?`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Custom AI agent by Ajay Vikram`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR CHAT HISTORY`;
+export const PAGE_TITLE: string = `AJAY'S AI ASSISTANT `;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
