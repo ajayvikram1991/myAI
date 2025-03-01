26b79559d79d17883ebd6b2f8615fa02e8d4abe7
ajayvikram1991 <ajay.vikram1991@gmail.com>
2025-03-01 03:57:05 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Ajay Vikram`;
 export const OWNER_DESCRIPTION: string = `I am a 2Y MBA Student at Kenan-Flagler Business School.`;
 
 export const AI_NAME: string = `Maverick`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_TONE: string = `Friendly`;
+export const AI_ROLE: string = `Personal Assistant`;
