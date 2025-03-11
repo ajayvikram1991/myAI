600fcd687433f1102ba2953a0893fe2152d73f22
ajayvikram1991 <ajay.vikram1991@gmail.com>
2025-03-11 01:55:43 -0400

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. I can answer questions related to Ajay's professional and MBA experience.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. I can answer questions related to Ajay's professional and MBA experience. Some of his key skills include Finance, Investments, Marketing, Consulting, Technology, Data Analysis, Strategy, Project Management, Sustainability & Energy`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `Sorry, that was too long. I need a break`;
